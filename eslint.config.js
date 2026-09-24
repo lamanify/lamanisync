5b092637ddfae3e08daaf755cf721efa6c12f2fd
Azri <lamanify@gmail.com>
2026-09-24 19:04:25 +0800

    chore: ignore website and astro artifacts in root eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist/**', 'node_modules/**', 'scripts/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'scripts/**', 'website/**', '**/.astro/**'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
